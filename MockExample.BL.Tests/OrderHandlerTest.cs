b0233d9fbbbc2d27f7f4d57184ca67a28208971a
Mikael Piotrowski <mishakun@gmail.com>
2012-04-23 14:11:10 +0200

    Implemented the usecase for OrderHandler with a manual mock

--- MockExample.BL.Tests/OrderHandlerTest.cs
+++ MockExample.BL.Tests/OrderHandlerTest.cs
@@ -5,13 +5,29 @@ namespace MockExample.BL.Tests
     [TestFixture]
     public class OrderHandlerTest
     {
+        // Mock implementation
+        public class MockedOrderRepository : IOrderRepository
+        {
+            public bool HasCalledStore = false;
+
+            public Order GivenOrder = null;
+
+            public void Store(Order o)
+            {
+                HasCalledStore = true;
+                GivenOrder = o;
+            }
+        }
 
         private OrderHandler _orderHandler;
+
+        private MockedOrderRepository _mockedOrderRepository;
 
         [SetUp]
         public void SetUp()
         {
             _orderHandler = new OrderHandler();
+            _mockedOrderRepository = new MockedOrderRepository();
         }
 
         [Test]
@@ -38,19 +54,24 @@ namespace MockExample.BL.Tests
             Assert.That(order, Is.Not.SameAs(_orderHandler.NewOrder()));
         }
 
+
         [Test]
         public void WhenOrderIsSubmittedShouldSaveItToDatabase()
         {
             // ARRANGE
             var order = _orderHandler.NewOrder();
             // Arrange the orderhandler to use a mock of IOrderRepository
+            _orderHandler.OrderRepository = _mockedOrderRepository;
 
             // ACT
             // Submit order through _orderHandler
+            _orderHandler.Submit(order);
 
             // ASSERT
             // Assert that OrderHandler object has saved it to the database
             // via the IOrderRepository interface
+            Assert.That(_mockedOrderRepository.HasCalledStore, Is.True);
+            Assert.That(_mockedOrderRepository.GivenOrder, Is.SameAs(order));
         }
 
     }
--- MockExample.BL/OrderHandler.cs
+++ MockExample.BL/OrderHandler.cs
@@ -5,9 +5,16 @@
     /// </summary>
     public class OrderHandler
     {
+        public IOrderRepository OrderRepository { get; set; }
+
         public Order NewOrder()
         {
             return new Order();
+        }
+
+        public void Submit(Order order)
+        {
+            OrderRepository.Store(order);
         }
     }
 }
